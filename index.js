40b54291391e46af1d65db1a3e3bff3416a76a9f
Julian Gruber <julian@juliangruber.com>
2014-11-07 09:02:01 +0100

    streams2

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var through = require('through');
+var Transform = require('stream').Transform;
+var inherits = require('util').inherits;
 
-module.exports = offset;
+module.exports = Offset;
+inherits(Offset, Transform);
 
-function offset (off) {
-  var i = 0;
-  return through(function (chunk) {
-    if (++i > off) this.queue(chunk);
-  });
+function Offset(off){
+  if (!(this instanceof Offset)) return new Offset(off);
+  Transform.call(this, { objectMode: true });
+  this._i = 0;
+  this._offset = off;
 }
+
+Offset.prototype._transform = function(chunk, _, done){
+  ++this._i > this._offset
+    ? done(null, chunk)
+    : done();
+};
